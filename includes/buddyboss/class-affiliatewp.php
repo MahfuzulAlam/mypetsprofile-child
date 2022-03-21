34e9bb3e29f4fc4e422b059c293288f7c9d0ff91
Mahfuz <a_mahfuzul@yahoo.com>
2022-03-21 23:10:43 +0600

    update affiliate plan

--- includes/buddyboss/class-affiliatewp.php
+++ includes/buddyboss/class-affiliatewp.php
@@ -29,7 +29,10 @@ class Affiliate_Directorist_Pricting_Plan extends Affiliate_WP_Base
      */
     public function __construct()
     {
-        add_action('after_bb_atpp_gifting_plan', array($this, 'add_pending_referral'), 10, 1);
+        add_action('after_bb_atpp_gifting_plan', array($this, 'iap_add_pending_referral'), 10, 1);
+        //add_action('init', array($this, 'atbdp_order_completed_test'));
+        add_action('atbdp_order_completed', array($this, 'atbdp_order_completed'), 10, 2);
+        //do_action('atbdp_order_completed', $order_data['ID'], $listing_id);
         //add_action('gform_post_payment_completed', array($this, 'mark_referral_complete'), 10, 2);
         //add_action('gform_post_payment_refunded', array($this, 'revoke_referral_on_refund'), 10, 2);
     }
@@ -37,9 +40,8 @@ class Affiliate_Directorist_Pricting_Plan extends Affiliate_WP_Base
     /**
      * Add pending referral
      */
-    public function add_pending_referral($order_info)
+    public function iap_add_pending_referral($order_info)
     {
-        //e_var_dump($order_info);
         $reference    = $order_info['order_id'];
         $affiliate_id = $this->get_affiliate_id($reference);
 
@@ -113,6 +115,59 @@ class Affiliate_Directorist_Pricting_Plan extends Affiliate_WP_Base
         } else {
             affiliate_wp()->utils->log('mark_referral_complete: The referral could not be found.', $referral);
         }
+    }
+
+    /**
+     * Pricing Plan On Order Completed
+     */
+
+    public function atbdp_order_completed_test()
+    {
+        file_put_contents(dirname(__FILE__) . '/log.json', json_encode($this->was_referred()));
+    }
+
+    public function atbdp_order_completed($order_id, $listing_id)
+    {
+
+        $reference    = $order_id;
+        $affiliate_id = $this->get_affiliate_id($reference);
+
+        // Block referral if not referred or affiliate ID is empty.
+        if (!$this->was_referred() && empty($affiliate_id)) {
+            return; // Referral not created because affiliate not referred and not a coupon.
+        }
+        file_put_contents(dirname(__FILE__) . '/log-1.json', json_encode($order_id));
+
+        // create draft referral.
+        $referral_id = $this->insert_draft_referral(
+            $affiliate_id,
+            array(
+                'reference'          => $reference,
+                'description'        => 'Pricing Plan',
+            )
+        );
+        if (!$referral_id) {
+            //$this->log('Draft referral creation failed.');
+            return;
+        }
+
+        $order_amount = get_post_meta($order_id, '_amount', true);
+
+        // Get the referral type we are creating.
+
+        $this->referral_type = 'sale';
+
+        $referral_total = $this->calculate_referral_amount($order_amount, $order_id);
+
+        $this->hydrate_referral(
+            $referral_id,
+            array(
+                'status'      => 'pending',
+                'amount'      => $referral_total
+            )
+        );
+
+        //$this->log(sprintf('Referral #%d updated successfully.', $referral_id));
     }
 
     /**
