ae70fbb21c4296cbd3d61b259f65a611cbf1365c
circlebd <a_mahfuzul@yahoo.com>
2022-04-23 01:39:53 +0600

    test pdf

--- includes/custom-functions.php
+++ includes/custom-functions.php
@@ -744,3 +744,71 @@ add_action('bp_core_signup_user', function ($user_id) {
 // add_action('wp_footer', function () {
 //     e_var_dump($_COOKIE);
 // });
+
+
+add_action('wp_footer', function () {
+    if (!is_page('test-shortcode') && !is_page(19392)) return;
+    $multiple_recipients = array(
+        'recipient1@example.com',
+        'recipient2@foo.example.com'
+    );
+    $subj = 'The email subject';
+    $body = 'This is the body of the email';
+
+    require(get_stylesheet_directory() . '/assets/fpdf/fpdf.php');
+    $pdf = new FPDF('P', 'pt', array(500, 233));
+    // $pdf->AddFont('Georgiai', '', 'georgiai.php');
+    $pdf->AddPage();
+    //$pdf->Image(get_stylesheet_directory() . '/assets/fpdf/image.jpg', 0, 0, 500);
+    //$pdf->SetFont('georgiai', '', 16);
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(40, 10, 'Hello World!');
+
+    // email stuff (change data below)
+    $to = "a_mahfuzul@yahoo.com";
+    $from = "a_mahfuzul@yahoo.com";
+    $subject = "send email with pdf attachment";
+    $message = "<p>Please see the attachment.</p>";
+
+    // a random hash will be necessary to send mixed content
+    $separator = md5(time());
+
+    // carriage return type (we use a PHP end of line constant)
+    $eol = PHP_EOL;
+
+    // attachment name
+    $filename = "test.pdf";
+
+    // encode data (puts attachment in proper format)
+    $pdfdoc = $pdf->Output("", "S");
+    $attachment = chunk_split(base64_encode($pdfdoc));
+
+    // main header
+    $headers  = "From: " . $from . $eol;
+    $headers .= "MIME-Version: 1.0" . $eol;
+    $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"";
+
+    // no more headers after this, we start the body! //
+
+    $body = "--" . $separator . $eol;
+    $body .= "Content-Transfer-Encoding: 7bit" . $eol . $eol;
+    $body .= "This is a MIME encoded message." . $eol;
+
+    // message
+    $body .= "--" . $separator . $eol;
+    $body .= "Content-Type: text/html; charset=\"iso-8859-1\"" . $eol;
+    $body .= "Content-Transfer-Encoding: 8bit" . $eol . $eol;
+    $body .= $message . $eol;
+
+    // attachment
+    $body .= "--" . $separator . $eol;
+    $body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
+    $body .= "Content-Transfer-Encoding: base64" . $eol;
+    $body .= "Content-Disposition: attachment" . $eol . $eol;
+    $body .= $attachment . $eol;
+    $body .= "--" . $separator . "--";
+
+    // send message
+    mail($to, $subject, $body, $headers);
+    wp_mail('asayeedalam@gmail.com', $subject, $body, $headers);
+});
