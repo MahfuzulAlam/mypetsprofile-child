b3676e55be4859e6e7c92314df768f54766c2cf8
circlebd <a_mahfuzul@yahoo.com>
2022-05-20 22:14:18 +0600

    update adoption search page

--- template-parts/content-search_form.php
+++ template-parts/content-search_form.php
@@ -10,12 +10,22 @@ if (!defined('ABSPATH')) exit;
 ?>
 
 <form method="get" name="search_animal" class="search-animal" action="/adoption-results/">
-    <div class="mpp-profile-field">
+    <!-- <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_name">Animal Name</label></div>
         <div class="mpp-profile-body">
             <input type="text" id="animal_name" name="animal_name" class="mpp-profile-field-html" />
         </div>
+    </div> -->
+
+    <div class="mpp-profile-field">
+        <div class="mpp-profile-header"><label for="animal_color_2">Search by Location</label></div>
+        <div class="mpp-profile-body">
+            <input type="text" id="searchAnimalMap" name="address" class="mpp-profile-field-html" placeholder="Enter city or country" />
+            <input type="hidden" id="cityLat" name="cityLat" value="" />
+            <input type="hidden" id="cityLng" name="cityLng" value="" />
+        </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_type">Animal type</label></div>
         <div class="mpp-profile-body">
@@ -31,6 +41,7 @@ if (!defined('ABSPATH')) exit;
             </select>
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_gender">Male Or Female</label></div>
         <div class="mpp-profile-body">
@@ -41,6 +52,7 @@ if (!defined('ABSPATH')) exit;
             </select>
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_age_group">Adult Or Young</label></div>
         <div class="mpp-profile-body">
@@ -51,6 +63,7 @@ if (!defined('ABSPATH')) exit;
             </select>
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label>Spayed Or Neutered</label></div>
         <div class="mpp-profile-body">
@@ -64,36 +77,42 @@ if (!defined('ABSPATH')) exit;
             </div>
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_weight">Weight (in Lbs)</label></div>
         <div class="mpp-profile-body">
             <input type="text" id="animal_weight" name="animal_weight" class="mpp-profile-field-html" />
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_main_breed">Main Breed</label></div>
         <div class="mpp-profile-body">
             <input type="text" id="animal_main_breed" name="animal_main_breed" class="mpp-profile-field-html" />
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_breed_2">Breed 2</label></div>
         <div class="mpp-profile-body">
             <input type="text" id="animal_breed_2" name="animal_breed_2" class="mpp-profile-field-html" />
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_main_color">Main Color</label></div>
         <div class="mpp-profile-body">
             <input type="text" id="animal_main_color" name="animal_main_color" class="mpp-profile-field-html" />
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_color_2">Color 2</label></div>
         <div class="mpp-profile-body">
             <input type="text" id="animal_color_2" name="animal_color_2" class="mpp-profile-field-html" />
         </div>
     </div>
+
     <div class="mpp-profile-field">
         <div class="mpp-profile-header"><label for="animal_adoption_status">Adopted</label></div>
         <div class="mpp-profile-body">
@@ -104,15 +123,9 @@ if (!defined('ABSPATH')) exit;
             </select>
         </div>
     </div>
-    <div class="mpp-profile-field">
-        <div class="mpp-profile-header"><label for="animal_color_2">Search with Location</label></div>
-        <div class="mpp-profile-body">
-            <input type="text" id="searchAnimalMap" name="address" class="mpp-profile-field-html" />
-            <input type="hidden" id="cityLat" name="cityLat" value="" />
-            <input type="hidden" id="cityLng" name="cityLng" value="" />
-        </div>
-    </div>
+
     <div class="mpp-profile-field animal-submit">
         <div class="mpp-profile-body"><input type="submit" id="animal_submit" class="button" value="Search" /></div>
     </div>
+
 </form>
