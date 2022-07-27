a10704a0df1cf3a53f93187fa56bb6eb97878915
MahfuzulAlam <a_mahfuzul@yahoo.com>
2022-07-27 19:00:11 +0600

    update housing field

--- includes/directorist/templates/listing-form/housing.php
+++ includes/directorist/templates/listing-form/housing.php
@@ -7,17 +7,21 @@
  */
 
 $placeholder = !empty($data['placeholder']) ? $data['placeholder'] : '';
-e_var_dump($data);
+
+$value = isset($data['value']) && !empty($data['value']) ? $data['value'] : '';
+$listing_title = !empty($value) ? get_the_title($value) : '';
+
 ?>
 
 <div class="directorist-form-group directorist-form-categories-field">
 
     <?php \Directorist\Directorist_Listing_Form::instance()->field_label_template($data); ?>
 
-    <select name="<?php echo $data['field-key']; ?>" id="<?php echo $data['field-type']; ?>" class="directorist-form-element" data-placeholder="<?php echo esc_attr($placeholder); ?>" <?php \Directorist\Directorist_Listing_Form::instance()->required($data); ?>>
+    <select name="<?php echo $data['field_key']; ?>" id="<?php echo $data['type']; ?>" class="directorist-form-element" data-placeholder="<?php echo esc_attr($placeholder); ?>" <?php \Directorist\Directorist_Listing_Form::instance()->required($data); ?>>
 
         <?php
-        echo '<option value="">' . esc_attr($placeholder) . '</option>';
+        if (!empty($value) && !empty($listing_title))
+            echo '<option value="' . $value . '" selected>' . esc_attr($listing_title) . '</option>';
         ?>
 
     </select>
